1a82c7f3e515acf00cb3e58358c8f5adf9c6b340
Braden Church <bradenchurch@gmail.com>
2020-06-23 20:48:26 -0600

    functioning crud app

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,36 +1,29 @@
 class Api::PostsController < ApplicationController
-
   def index
     render json: Post.all
   end
-
   def create
     @post = Post.new(post_params)
     if @post.save
       render json: @post
     else
-      render json: { errors: @post.errors }, status: :unprocessable_entity
+      render json: { errors: @post.errors}, status: :unprocessable_entity
     end
   end
-
-  def update
+  def update 
     @post = Post.find(params[:id])
-    if @post.save
+    if @post.update(post_params)
       render json: @post
     else
-      render json: { errors: @post.errors }, status: :unprocessable_entity
+      render json: { errors: @post.errors}, status: :unprocessable_entity
     end
   end
-
   def destroy
     Post.find(params[:id]).destroy
-    render json: { message: 'Post Deleted'}
+    render json: { message: 'post deleted'}
   end
-
   private
-
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
-
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
